a86f1863fd758c1889ced7a96cfcbd1e688bf3db
poilerly <poiler@163.com>
2017-05-26 16:51:33 +0800

    Signed-off-by: poilerly <poiler@163.com>

--- cpu/arm920t/s3c24x0/nand_flash.c
+++ cpu/arm920t/s3c24x0/nand_flash.c
@@ -11,26 +11,28 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
-#define S3C2410_NFSTAT_READY	(1<<0)
-#define S3C2410_NFCONF_nFCE		(1<<11)
+#define S3C2410_NFSTAT_READY    (1<<0)
+#define S3C2410_NFCONF_nFCE     (1<<11)
 
-#define S3C2440_NFSTAT_READY	(1<<0)
-#define S3C2440_NFCONF_nFCE		(1<<1)
+#define S3C2440_NFSTAT_READY    (1<<0)
+#define S3C2440_NFCONF_nFCE     (1<<1)
+
 
 /* S3C2410:NAND Flash的片选函数 */
 static void s3c2410_nand_select_chip(struct mtd_info *mtd, int chip)
 {
-    S3C2410_NAND * const s3c2410nand = S3C2410_GetBase_NAND();
+	S3C2410_NAND *const s3c2410nand = S3C2410_GetBase_NAND();
 
-    if(chip == -1)
-    {
+	if (chip == -1)
+	{
 		s3c2410nand->NFCONF |= S3C2410_NFCONF_nFCE; //禁止片选信号
-    }
-    else
+	}
+	else
 	{
 		s3c2410nand->NFCONF &= ~S3C2410_NFCONF_nFCE; //使能片选信号
-    }
+	}
 }
+
 
 /* S3C2410:命令和控制函数
  *
@@ -39,35 +41,35 @@ static void s3c2410_nand_select_chip(struct mtd_info *mtd, int chip)
  * 真正发出命令是在上一层的NAND Flash的统一的驱动中实现,
  * 它首先调用这个函数修改"写地址",然后才分别发出控制,地址,数据序列.
  */
-
 static void s3c2410_nand_hwcontrol(struct mtd_info *mtd, int cmd)
 {
-	S3C2410_NAND * const s3c2410nand = S3C2410_GetBase_NAND();
+	S3C2410_NAND *const s3c2410nand = S3C2410_GetBase_NAND();
 	struct nand_chip *chip = mtd->priv;
 
-	switch(cmd)
+	switch (cmd)
 	{
-	case NAND_CTL_SETNCE:
-	case NAND_CTL_CLRNCE:
-		printf("%s: called for NCE\n",__FUNCTION__);
-		break;
+		case NAND_CTL_SETNCE:
+		case NAND_CTL_CLRNCE:
+			printf("%s: called for NCE\n", __FUNCTION__);
+			break;
 
-	case NAND_CTL_SETCLE:
-		chip->IO_ADDR_W = (void *)&s3c2410nand->NFCMD;
-		break;
+		case NAND_CTL_SETCLE:
+			chip->IO_ADDR_W = (void *)&s3c2410nand->NFCMD;
+			break;
 
-	case NAND_CTL_SETALE:
-		chip->IO_ADDR_W = (void *)&s3c2410nand->NFADDR;
-		break;
+		case NAND_CTL_SETALE:
+			chip->IO_ADDR_W = (void *)&s3c2410nand->NFADDR;
+			break;
 
-		/* NAND_CTL_CLRCLE: */
-		/* NAND_CTL_CLRALE: */
+			/* NAND_CTL_CLRCLE: */
+			/* NAND_CTL_CLRALE: */
 
-	default:
-		chip->IO_ADDR_W =  (void *)&s3c2410nand->NFDATA;
-		break;
+		default:
+			chip->IO_ADDR_W =  (void *)&s3c2410nand->NFDATA;
+			break;
 	}
 }
+
 
 /* S3C2410:查询NAND Flash状态
  *
@@ -75,7 +77,7 @@ static void s3c2410_nand_hwcontrol(struct mtd_info *mtd, int cmd)
  */
 static void s3c2410_nand_devready(struct mtd_info *mtd, int cmd)
 {
-	S3C2410_NAND * const s3c2410nand = S3C2410_GetBase_NAND();
+	S3C2410_NAND *const s3c2410nand = S3C2410_GetBase_NAND();
 
 	return (s3c2410nand->NFSTAT & S3C2410_NFSTAT_READY);
 }
@@ -84,17 +86,18 @@ static void s3c2410_nand_devready(struct mtd_info *mtd, int cmd)
 /* S3C2440:NAND Flash的片选函数 */
 static void s3c2440_nand_select_chip(struct mtd_info *mtd, int chip)
 {
-    S3C2440_NAND * const s3c2440nand = S3C2440_GetBase_NAND();
+	S3C2440_NAND *const s3c2440nand = S3C2440_GetBase_NAND();
 
-    if(chip == -1)
-    {
+	if (chip == -1)
+	{
 		s3c2440nand->NFCONF |= S3C2440_NFCONF_nFCE; //禁止片选信号
-    }
-    else
-    {
+	}
+	else
+	{
 		s3c2440nand->NFCONF &= ~S3C2440_NFCONF_nFCE; //使能片选信号
-    }
+	}
 }
+
 
 /* S3C2440:命令和控制函数
  *
@@ -103,35 +106,35 @@ static void s3c2440_nand_select_chip(struct mtd_info *mtd, int chip)
  * 真正发出命令是在上一层的NAND Flash的统一的驱动中实现,
  * 它首先调用这个函数修改"写地址",然后才分别发出控制,地址,数据序列.
  */
-
 static void s3c2440_nand_hwcontrol(struct mtd_info *mtd, int cmd)
 {
-	S3C2440_NAND * const s3c2440nand = S3C2440_GetBase_NAND();
+	S3C2440_NAND *const s3c2440nand = S3C2440_GetBase_NAND();
 	struct nand_chip *chip = mtd->priv;
 
-	switch(cmd)
+	switch (cmd)
 	{
-	case NAND_CTL_SETNCE:
-	case NAND_CTL_CLRNCE:
-		printf("%s: called for NCE\n",__FUNCTION__);
-		break;
+		case NAND_CTL_SETNCE:
+		case NAND_CTL_CLRNCE:
+			printf("%s: called for NCE\n", __FUNCTION__);
+			break;
 
-	case NAND_CTL_SETCLE:
-		chip->IO_ADDR_W = (void *)&s3c2440nand->NFCMD;
-		break;
+		case NAND_CTL_SETCLE:
+			chip->IO_ADDR_W = (void *)&s3c2440nand->NFCMD;
+			break;
 
-	case NAND_CTL_SETALE:
-		chip->IO_ADDR_W = (void *)&s3c2440nand->NFADDR;
-		break;
+		case NAND_CTL_SETALE:
+			chip->IO_ADDR_W = (void *)&s3c2440nand->NFADDR;
+			break;
 
-		/* NAND_CTL_CLRCLE: */
-		/* NAND_CTL_CLRALE: */
+			/* NAND_CTL_CLRCLE: */
+			/* NAND_CTL_CLRALE: */
 
-	default:
-		chip->IO_ADDR_W =  (void *)&s3c2440nand->NFDATA;
-		break;
+		default:
+			chip->IO_ADDR_W =  (void *)&s3c2440nand->NFDATA;
+			break;
 	}
 }
+
 
 /* S3C2440:查询NAND Flash状态
  *
@@ -139,7 +142,7 @@ static void s3c2440_nand_hwcontrol(struct mtd_info *mtd, int cmd)
  */
 static void s3c2440_nand_devready(struct mtd_info *mtd, int cmd)
 {
-	S3C2440_NAND * const s3c2440nand = S3C2440_GetBase_NAND();
+	S3C2440_NAND *const s3c2440nand = S3C2440_GetBase_NAND();
 
 	return (s3c2440nand->NFSTAT & S3C2440_NFSTAT_READY);
 }
@@ -151,34 +154,35 @@ static void s3c2440_nand_devready(struct mtd_info *mtd, int cmd)
  */
 static void s3c24x0_nand_inithw(void)
 {
-	S3C2410_NAND * const s3c2410nand = S3C2410_GetBase_NAND();
-	S3C2440_NAND * const s3c2440nand = S3C2440_GetBase_NAND();
+	S3C2410_NAND *const s3c2410nand = S3C2410_GetBase_NAND();
+	S3C2440_NAND *const s3c2440nand = S3C2440_GetBase_NAND();
 
-#define TACLS		0
-#define TWRPH0		4
-#define TWRPH1		2
+#define TACLS       0
+#define TWRPH0      4
+#define TWRPH1      2
 
-	if(gd->bd->bi_arch_munber == MACH_TYPE_SMDK2410)
+	if (gd->bd->bi_arch_number == MACH_TYPE_SMDK2410)
 	{
 		/* 使能NAND Flash控制器, 初始化ECC, 禁止片选, 设置时序 */
-        s3c2410nand->NFCONF = (1<<15)|(1<<12)|(1<<11)|(TACLS<<8)|(TWRPH0<<4)|(TWRPH1<<0);
+		s3c2410nand->NFCONF = (1 << 15) | (1 << 12) | (1 << 11) | (TACLS << 8) | (TWRPH0 << 4) | (TWRPH1 << 0);
 	}
 	else
 	{
 		/* 设置时序 */
-        s3c2440nand->NFCONF = (TACLS<<12)|(TWRPH0<<8)|(TWRPH1<<4);
-        /* 使能NAND Flash控制器, 初始化ECC, 禁止片选 */
-        s3c2440nand->NFCONT = (1<<4)|(1<<1)|(1<<0);
+		s3c2440nand->NFCONF = (TACLS << 12) | (TWRPH0 << 8) | (TWRPH1 << 4);
+		/* 使能NAND Flash控制器, 初始化ECC, 禁止片选 */
+		s3c2440nand->NFCONT = (1 << 4) | (1 << 1) | (1 << 0);
 	}
 }
+
 
 /*
  * 被drivers/nand/nand.c 调用,初始化NAND Flash硬件,初始化访问接口函数
  */
 void board_nand_init(struct nand_chip *chip)
 {
-	S3C2410_NAND * const s3c2410nand = S3C2410_GetBase_NAND();
-	S3C2440_NAND * const s3c2440nand = S3C2440_GetBase_NAND();
+	S3C2410_NAND *const s3c2410nand = S3C2410_GetBase_NAND();
+	S3C2440_NAND *const s3c2440nand = S3C2440_GetBase_NAND();
 
 	s3c24x0_nand_inithw(); //NAND Flash硬件初始化
 
@@ -202,7 +206,6 @@ void board_nand_init(struct nand_chip *chip)
 	}
 	chip->eccmode = NAND_ECC_SOFT; //ECC校验方式:软件ECC
 }
-
 
 
 #endif
